e4f9a0437f431ee9a657da8dcec41e99b7753055
vahid <vahid_heydari68@yahoo.com>
2020-09-05 19:26:25 +0430

    First commit

---
+++ src/main/java/net/javaguides/hibernate/TestConnection.java
@@ -0,0 +1,24 @@
+package net.javaguides.hibernate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Created by v.heydari on
+ * 8/22/2020 4:43 PM
+ */
+
+public class TestConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306";
+        String username = "root";
+        String password = "Vahid@123";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("connection successful...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
