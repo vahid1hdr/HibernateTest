52f505cd8518e4b4a68466b6dd2f1945a17b6e91
vahid <vahid_heydari68@yahoo.com>
2020-09-06 18:03:44 +0430

    test commit

---
+++ src/main/java/net/javaguides/hibernate/entity/Person.java
@@ -0,0 +1,36 @@
+package net.javaguides.hibernate.entity;
+
+/**
+ * Created by v.heydari on
+ * 9/6/2020 6:02 PM
+ */
+
+public class Person {
+    private int age;
+    private String firstName;
+    private String lastName;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
